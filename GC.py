6424b480e3a815a9299620c68bbbdee721214a37
Sangharsh_1312 <zaemon1312@gmail.com>
2025-10-30 20:36:17 +0530

    Add files via upload

---
+++ GC.py
@@ -0,0 +1,41 @@
+# age = int(input("Enter your age: "))
+
+# if age > 18:
+#     print("You can vote.")
+
+# elif age == 18:
+#     print("Congrates, You are now eligible to vote.")
+
+# else:
+#     print("Sorry,you can not vote yet.")
+
+# eo = int(input("Enter any number: "))
+
+# if eo % 2 == 0:
+#     print("The number is the Even number.")
+
+# else:
+#     print("the number is the Odd nummber.")
+
+
+# GRADE CALCULATOR  
+
+m = int(input("Enter your marks: "))
+
+if m > 90:
+    print("Congrates, your grade is A...")
+
+elif m > 80:
+    print("Your grade is B.")
+
+elif m > 70:
+    print("Your grage is C.")
+
+elif m > 60:
+    print("Your grade is D.")
+
+elif m > 40:
+    print("Your grade is E.")
+
+else:
+    print("Your grade is F. Better luck next time.")
